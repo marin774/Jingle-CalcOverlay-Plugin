1395285a19f4425a5fb7491f90d52893565984eb
marin774 <marinbanozic@gmail.com>
2024-10-02 15:22:36 +0200

    fix angle (copied from ninjabrainbot)

--- src/main/java/me/marin/calcoverlay/util/AngleToCoords.java
+++ src/main/java/me/marin/calcoverlay/util/AngleToCoords.java
@@ -8,50 +8,18 @@ public class AngleToCoords {
     private final double actualAngle;
     private final double neededAngleCorrection;
 
-    public static AngleToCoords from(Prediction prediction, PlayerPosition playerPosition) {
-        double x;
-        double z;
-        double strongholdX;
-        double strongholdZ;
-        if (!playerPosition.isInNether()) {
-            x = playerPosition.getXInOverworld();
-            z = playerPosition.getZInOverworld();
+    public static AngleToCoords from(Prediction prediction, PlayerPosition playerPos) {
+        //boolean playerIsInNether = playerPos.isInNether();
+        double xDiff = prediction.getChunkX()*16+4 - playerPos.getXInOverworld();
+        double zDiff = prediction.getChunkZ()*16+4 - playerPos.getZInOverworld();
+        double angleToStructure = -Math.atan2(xDiff, zDiff) * 180 / Math.PI;
+        double angleDifference = (angleToStructure - playerPos.getHorizontalAngle()) % 360;
+        if (angleDifference > 180)
+            angleDifference -= 360;
+        if (angleDifference < -180)
+            angleDifference += 360;
 
-            strongholdX = prediction.getChunkX() * 16 + 4.5;
-            strongholdZ = prediction.getChunkZ() * 16 + 4.5;
-        } else {
-            x = playerPosition.getXInOverworld() / 8;
-            z = playerPosition.getZInOverworld() / 8;
-
-            strongholdX = prediction.getChunkX() * 2 + 0.5;
-            strongholdZ = prediction.getChunkZ() * 2 + 0.5;
-        }
-
-        double actualAngle = Math.toDegrees(Math.atan2(strongholdX - x, z - strongholdZ)) + 180;
-        while (actualAngle > 180) {
-            actualAngle -= 360;
-        }
-        while (actualAngle < -180) {
-            actualAngle += 360;
-        }
-
-        double playerAngle = playerPosition.getHorizontalAngle();
-        while (playerAngle > 180) {
-            playerAngle -= 360;
-        }
-        while (playerAngle < -180) {
-            playerAngle += 360;
-        }
-
-        double neededAngleCorrection = actualAngle - playerAngle;
-        while (neededAngleCorrection > 180) {
-            neededAngleCorrection -= 360;
-        }
-        while (neededAngleCorrection < -180) {
-            neededAngleCorrection += 360;
-        }
-
-        return new AngleToCoords(actualAngle, neededAngleCorrection);
+        return new AngleToCoords(angleToStructure, angleDifference);
     }
 
 }
